ca542ea788e62d739eb8438b08b6c1c99e306e3f
yuki <172404460@qq.com>
2017-06-02 10:15:25 +0800

    第一次修改,可能有问题
    alert不出来想要的值

--- main/main.js
+++ main/main.js
@@ -1,7 +1,10 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.numm()}
+o) 平均值 = ${sequence.average()}
+
 `);
 };
 
@@ -9,10 +12,46 @@ class Sequence {
   constructor(input) {
     // Write your code here
   }
-
+   numm()
+   {
+   	return input.length;
+   }
   minimum() {
-    // Write your code here
+    var mins=0; 
+    for(let i=0;i<input.length;i++)
+    {
+
+     if (mins>input[i])
+     {
+      mins=input[i];
+     }
+
+    }
+    
+    return mins;
   }
+  maximum()
+  {
+    var maxs;
+    for(let i=0;i<input.length;i++)
+    {
+      if(maxs<input[i])
+      {
+        maxs=input[i];
+      }
+    }
+    return maxs;
+  }
+  average()
+  {
+    var ave=0;
+    for(let i=0;i<input.length;i++)
+    {
+      ave=ave+input[i];
+    }
+    return  ave.toFixed(2);
+  }
+  // Write your code here
 
   // Write your code here
 }
